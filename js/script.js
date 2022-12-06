2b33b1efd056d349bb3bcf0020327dec2f8f2fa5
Ming-Yang Lo <benji860905@gmail.com>
2022-12-05 21:12:52 -0500

    Final Update VI

--- js/script.js
+++ js/script.js
@@ -13,3 +13,32 @@ function textAreaHeightAdjust(textArea) {
         height+=6;
     }
 }
+
+
+var i = 0;
+function textAreaCollapse(){
+    var formSpan = document.getElementById("gallery_form").getElementsByTagName("span")[0];
+    var fontSize = window.getComputedStyle(formSpan).fontSize.split('p')[0];
+    if(Number(fontSize)==18){
+        var textArea = document.getElementById("gallery_form").getElementsByTagName("textarea")[0];
+        var button = document.getElementById("gallery_form").getElementsByTagName("button")[0];
+        i+=1;
+        if((i%2)==1){
+            textArea.style.display = "none";
+            button.style.display = "none";
+            document.getElementById("up").style.display="initial";
+            document.getElementById("up_two").style.display="initial";
+            document.getElementById("down").style.display="none";
+            document.getElementById("down_two").style.display="none";
+        }
+        else{
+            textArea.style.display = "initial";
+            button.style.display = "initial";
+            document.getElementById("up").style.display="none";
+            document.getElementById("up_two").style.display="none";
+            document.getElementById("down").style.display="initial";
+            document.getElementById("down_two").style.display="initial";
+        }
+    }
+}
+document.getElementById("clickable").addEventListener("click", textAreaCollapse);
